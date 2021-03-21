885a64c9a9714502c846c10c4423963eef68aa31
ivanmartinezv <ivmartez@gmail.com>
2021-03-21 03:20:04 -0300

    5 files modified

--- src/app/componentes/campeon.component.ts
+++ src/app/componentes/campeon.component.ts
@@ -10,6 +10,8 @@ import {
 //modelos
 
 //servicios
+import { AspectoService } from "../servicios/aspecto.service";
+import { CampeonService } from "../servicios/campeon.service";
 
 @Component({
   selector: "app-campeon",
@@ -19,9 +21,55 @@ import {
 
 //COMPONENTE CREADO PARA IMPLEMENTAR UN MODELO NO RELACIONAL DE DATOS
 export class CampeonComponent implements OnInit {
-  constructor() {}
+  public titulo: string = "Titulo del campeon component";
+  public contador_obtenibles: number = 0;
+  public contador_posesion: number = 0;
+  public contador_botin: number = 0;
+
+  public campeones: any = [
+    {
+      id: 1,
+      nombre: "Aatrox",
+      aspectos: [
+        {
+          nombre_aspecto: "Aatrox Luna de Sangre",
+          tipo: "Epica",
+          precio: 1350,
+          obtenible: true,
+          posesion: false,
+          botin: false,
+          id_campeon: 1
+        }
+      ],
+      cont_obtenible: 0,
+      cont_posesion: 0,
+      cont_botin: 0
+    },
+    {
+      id: 2,
+      nombre: "Ahri",
+      aspectos: [],
+      cont_obtenible: 0,
+      cont_posesion: 0,
+      cont_botin: 0
+    }
+  ];
+
+  constructor(
+    private _campeonService: CampeonService,
+    private _aspectoService: AspectoService
+  ) {}
 
   ngOnInit() {
     console.log("ngOnInit()");
   }
+
+  lecturaDatosEstaticos() {
+    let largo: number = 10;
+    for (let i = 0; i < largo; i++) {
+      this.campeones[i] = "perro";
+    }
+  }
+
+  lecturaDatosFirebase() {}
 }
